80908008c644648db9d75362ecfba3a5a811855b
Finn Kiesinger <finnkiesinger@gmail.com>
2024-02-14 10:51:33 +0100

    commit

--- lib/views/dashboard/facility/storage_blueprint/storage_blueprint_list.dart
+++ lib/views/dashboard/facility/storage_blueprint/storage_blueprint_list.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
+import '../../../../models/crafting/storage_facility_blueprint.dart';
 import '../../../../models/util/game_state.dart';
 
 class StorgeBlueprintList extends StatefulWidget {
@@ -16,6 +17,26 @@ class _StorgeBlueprintListState extends State<StorgeBlueprintList> {
     var game = context.watch<GameState>();
     var blueprints =
         game.blueprints.whereType<StorageFacilityBlueprint>().toList();
-    return;
+    return ListView.separated(
+      itemBuilder: (context, index) {
+        return Container();
+      },
+      separatorBuilder: (context, index) {
+        return Padding(
+          padding: const EdgeInsets.symmetric(vertical: 48),
+          child: Center(
+            child: Container(
+              height: 2,
+              width: 150,
+              decoration: BoxDecoration(
+                color: Colors.white10,
+                borderRadius: BorderRadius.circular(2.0),
+              ),
+            ),
+          ),
+        );
+      },
+      itemCount: blueprints.length,
+    );
   }
 }
